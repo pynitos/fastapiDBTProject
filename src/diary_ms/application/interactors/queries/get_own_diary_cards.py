ec9520d9597461e36aea9ecc8875690676db5a3e
monolito-s <129774792+monolito-s@users.noreply.github.com>
2024-11-04 17:58:09 +0300

    domain

--- src/diary_ms/application/interactors/queries/get_own_diary_cards.py
+++ src/diary_ms/application/interactors/queries/get_own_diary_cards.py
@@ -2,7 +2,7 @@ from src.diary_ms.application.dto.diary_card import GetOwnDiaryCardsDTO
 from src.diary_ms.application.interfaces.gateway import ReaderProtocol
 from src.diary_ms.application.interfaces.id_provider import IdProvider
 from src.diary_ms.application.interfaces.interactor import Interactor
-from src.diary_ms.domain.model.entities import DiaryCardDM
+from src.diary_ms.domain.model.aggregates.diary_card import DiaryCardDM
 
 
 class GetOwnDiaryCards(Interactor[GetOwnDiaryCardsDTO, list[DiaryCardDM]]):
@@ -14,7 +14,7 @@ class GetOwnDiaryCards(Interactor[GetOwnDiaryCardsDTO, list[DiaryCardDM]]):
         self.db_gateway = db_gateway
         self.id_provider = id_provider
 
-    def __call__(self, data: GetOwnDiaryCardsDTO) -> DiaryCardDM:
+    def __call__(self, data: GetOwnDiaryCardsDTO) -> list[DiaryCardDM]:
         diary_cards: list[DiaryCardDM] = self.db_gateway.get_all(offset=data.pagination.offset,
                                                                  limit=data.pagination.limit)
         return diary_cards
