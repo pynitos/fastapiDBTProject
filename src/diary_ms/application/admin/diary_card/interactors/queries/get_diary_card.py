60ad85659beb7a12029c169bd1f2f44053bb0e20
monolito-s <129774792+monolito-s@users.noreply.github.com>
2025-02-08 19:26:59 +0300

    .

--- src/diary_ms/application/admin/diary_card/interactors/queries/get_diary_card.py
+++ src/diary_ms/application/admin/diary_card/interactors/queries/get_diary_card.py
@@ -1,7 +1,8 @@
 from src.diary_ms.application.admin.diary_card.dto.diary_card import DiaryCardAdminDTO, GetDiaryCardAdminDTO
 from src.diary_ms.application.admin.diary_card.dto.mappers.diary_card import DiaryCardAdminDTOMapper
+from src.diary_ms.application.admin.diary_card.interfaces.gateway import DiaryCardAdminReader
 from src.diary_ms.application.common.interfaces.handlers.query import QueryHandler
-from src.diary_ms.application.common.interfaces.id_provider import IdProvider
+from src.diary_ms.application.common.interfaces.id_provider import AdminIdProvider, IdProvider
 from src.diary_ms.application.diary_card.exceptions.diary_card import DiaryCardNotFoundError
 from src.diary_ms.application.diary_card.interfaces.gateway import DiaryCardReader
 from src.diary_ms.domain.model.aggregates.diary_card import DiaryCard
@@ -10,15 +11,15 @@ from src.diary_ms.domain.model.entities.user_id import UserId
 
 
 class GetDiaryCardAdminHandler(QueryHandler[GetDiaryCardAdminDTO, DiaryCardAdminDTO]):
-    def __init__(self, db_gateway: DiaryCardReader, id_provider: IdProvider):
+    def __init__(self, db_gateway: DiaryCardAdminReader, id_provider: AdminIdProvider):
         self._db_gateway = db_gateway
         self._id_provider = id_provider
         self._mapper = DiaryCardAdminDTOMapper
 
     async def __call__(self, query: GetDiaryCardAdminDTO) -> DiaryCardAdminDTO:
-        user_id: UserId = self._id_provider.get_current_user_id()
+        self._id_provider.get_admin_user_id()
         id: DiaryCardId = DiaryCardId(query.id)
-        diary_card: DiaryCard | None = await self._db_gateway.get_by_id(id, user_id)
+        diary_card: DiaryCard | None = await self._db_gateway.get_by_id(id)
         if not diary_card:
             raise DiaryCardNotFoundError
         return self._mapper.dm_to_dto(diary_card)
